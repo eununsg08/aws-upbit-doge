dae5be98ee47661083da7bddbc94b28a593790f4
eununsg08 <69507865+eununsg08@users.noreply.github.com>
2021-09-03 19:32:49 +0900

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pyupbit
-
-access = "9LmhrSzVNr0ELpsrDoBVCpU2sAcncQcZVHzQuRRW"
-secret = "xV0oZoFfPAqrKHyEv28OfyUp9mBTzfu3uisgQqud"
-upbit = pyupbit.Upbit(access, secret)
-
-print(upbit.get_balance("KRW-DOGE"))     # KRW-XRP 조회
-print(upbit.get_balance("KRW"))         # 보유 현금 조회
